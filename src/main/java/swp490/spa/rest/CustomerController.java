e28cdd26d346a753b4999388ec669d10d71375b2
Phong Pham <phong.pham29@gmail.com>
2021-07-16 15:38:14 +0700

    EDIT: notification when booking

--- src/main/java/swp490/spa/rest/CustomerController.java
+++ src/main/java/swp490/spa/rest/CustomerController.java
@@ -507,13 +507,12 @@ public class CustomerController {
                 Booking bookingInsert = bookingService.insertNewBooking(booking);
                 if(Objects.nonNull(bookingInsert)){
                     try {
-                        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
                         List<Manager> managers =
                                 managerService.findManagerBySpa(spa.getId());
                         Map<String, String> map = new HashMap<>();
                         map.put(MessageTemplate.BOOKING_TITLE, bookingInsert.getId().toString());
                         if(notificationFireBaseService.notify(MessageTemplate.BOOKING_TITLE,
-                                String.format(MessageTemplate.BOOKING_MESSAGE,formatter.format(LocalTime.now())),
+                                String.format(MessageTemplate.BOOKING_MESSAGE,LocalTime.now().toString()),
                                 map, managers.get(0).getUser().getId(), Role.MANAGER)){
                             return ResponseHelper.ok(String.format(LoggingTemplate.INSERT_SUCCESS, Constant.BOOKING));
                         }
